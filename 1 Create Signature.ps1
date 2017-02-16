0c55220450a7c30a62db6f6c5c8ae573988f61b0
Ruelle <joelle.Ruelle@smartview.fr>
2017-02-16 16:25:06 +0100

    V1

---
+++ 1 Create Signature.ps1
@@ -0,0 +1,45 @@
+﻿#Create authentification object for the user
+$credential = Get-Credential -UserName 'your login' -Message "Enter SPO credentials"
+
+#Initializing a persistent connection to Exchange
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRed
+
+#Import of the session in the current session
+Import-PSSession $Session -AllowClobber
+
+#Connect to Azure Active Directory with credentials 
+Connect-MsolService -Credential $credential
+
+#########################################################################################################################
+
+#Save directory for my signatures
+$save_location = "your save path"
+
+#Get all users 
+$users = Get-MsolUser 
+ 
+foreach ($user in $users) {
+ 
+  $DisplayName= “$($user.DisplayName)”
+  $title = "$($User.Title)"
+  $MobilePhone = "$($User.MobilePhone)"
+  $UserPrincipalName = "$($User.UserPrincipalName)"
+
+  #Create and save personnal signature in HTML
+  $output_file = $save_location + $DisplayName + ".html"
+  Write-Host "Création de la signature au format html pour " $DisplayName
+   "<span style=`"font-family: calibri,sans-serif;`"><strong>" + $DisplayName + "</strong><br />", $title + " - " + $MobilePhone + "<br />", $UserPrincipalName + "<br />", "</span><br />"| Out-File $output_file
+}
+
+# Get user by DisplayName
+$Myuser = Get-MsolUser  | Where-Object {$_.DisplayName -eq "user display name"}
+$MyuserDisplayName= “$($Myuser.DisplayName)”
+
+#Assign signature to the previously selected user using the HTML file
+$output_file_user = $save_location + $MyuserDisplayName + ".html"
+$signHTML = (Get-Content $output_file_user)
+Set-MailboxMessageConfiguration –Identity $Myuser.UserPrincipalName -AutoAddSignature $True  -SignatureHtml   $signHTML
+
+##################################################################################################################################################################################################################
+#Log out of Office 365
+get-PSSession | remove-PSSession
---
+++ 2 Get Skype & Exchange License.ps1
@@ -0,0 +1,45 @@
+﻿#Create authentification object for the user
+$credential = Get-Credential -UserName 'your login' -Message "Enter SPO credentials"
+
+#Import Lync module
+Import-Module LyncOnlineConnector
+
+#Log in to Office 365 and register using the information provided to previously.
+$session = New-CsOnlineSession -Credential $credential
+
+#Import O365 user session in the current session
+Import-PSSession $session -AllowClobber
+
+#Initializing a persistent connection to Exchange
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRed
+
+#Import Exchange user session in the current session
+Import-PSSession $Session -AllowClobber
+
+#Connect to Azure Active Directory with credentials 
+Connect-MsolService -Credential $credential
+
+#Get users with a license
+$x = Get-MsolUser  | Where-Object {$_.isLicensed -eq "TRUE"} 
+
+
+foreach ($i in $x)
+    {
+      #Getting information on the user's mailbox and store it in the object $i
+      $y = Get-Mailbox -Identity $i.UserPrincipalName
+      Write-Host $i.UserPrincipalName
+      If($y)
+      {
+        #Adding the information IsMailboxEnabled in the object $i
+        $i | Add-Member -MemberType NoteProperty -Name IsMailboxEnabled -Value $y.IsMailboxEnabled
+      }
+
+      #Retrieving information about the Lync account of the user stored in the object $i
+      $y = Get-CsOnlineUser -Identity $i.UserPrincipalName
+
+      #Adding the information  EnabledForSkype in the object $i
+      $i | Add-Member -MemberType NoteProperty -Name EnabledForSkype -Value $y.Enabled
+    }
+
+#Display informations obtained above
+$x | Select-Object DisplayName, PrimaryEmailAddress, IsLicensed, IsMailboxEnabled, EnabledForSkype | Sort-Object DisplayName| Export-Csv "your path\file.csv" -Delimiter ";"
